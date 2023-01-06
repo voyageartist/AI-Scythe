383562c1b28c9dd54cda44c9dfc519e907e8d25d
voyageartist <voyageartist@proton.me>
2023-01-06 16:06:03 +0000

    added common core.css usage

--- R/api.r
+++ R/api.r
@@ -6,3 +6,9 @@ install.packages("devtools")
 
 library(Rcpp)
 library(devtools)
+
+Rcpp.package.skeleton("scythe")
+
+"TODO : load the lib and wrap the C functions"
+PATH <- "C:/Users/Xanto183/git/Scythe/src/scythe.lib";
+dyn.load(PATH);
