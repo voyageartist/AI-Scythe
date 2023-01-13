d4852e4cdcda6071c853a644c995d0bb4933cd27
voyageartist <voyageartist@proton.me>
2023-01-13 13:49:30 +0000

    swift 5

--- R/scythe/src/api.cpp
+++ R/scythe/src/api.cpp
@@ -1,1 +1,11 @@
-#i
+#include "RcppArmadillo.h"
+// [[Rcpp::depends(RcppArmadillo)]]
+
+#include "scythe.hpp"
+
+// [[Rcpp::export]]
+double foo() {
+	double a;
+	std::cout << "Hello world !" << std::endl;
+	return a;
+}
