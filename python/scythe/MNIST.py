66be7d30c15ec521cae2860b0e6b8bcf83d949ec
voyageartist <voyageartist@proton.me>
2023-01-15 17:02:12 +0000

    Update hosts.txt

--- python/scythe/MNIST.py
+++ python/scythe/MNIST.py
@@ -3,4 +3,12 @@
 # author : Antoine Passemiers
 
 import os, sys, struct
-import
+import numpy as np
+
+
+def loadMNISTTrainingSet(location = "."):
+
+    y_filepath = os.path.join(location, "train-labels-idx1-ubyte")
+    with open(y_filepath, "rb") as y_file:
+        _, _ = struct.unpack(">II", y_file.read(8))
+        
