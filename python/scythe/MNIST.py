4b1396f7099ee8fbd232ca1ff614d989dcfc4bbd
voyageartist <voyageartist@proton.me>
2023-01-16 19:19:31 +0000

    Update canales_tv

--- python/scythe/MNIST.py
+++ python/scythe/MNIST.py
@@ -11,4 +11,8 @@ def loadMNISTTrainingSet(location = "."):
     y_filepath = os.path.join(location, "train-labels-idx1-ubyte")
     with open(y_filepath, "rb") as y_file:
         _, _ = struct.unpack(">II", y_file.read(8))
-        
+        labels = np.fromfile(y_file, dtype = np.int8)
+
+    X_filepath = os.path.join(location, "train-images-idx3-ubyte")
+    with open(X_filepath, "rb") as X_file:
+        _, _, instances, features = struct.unpack(">IIII", X_file.re
