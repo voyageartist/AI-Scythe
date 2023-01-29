b7748e42ed9d8b4cf740179367f127769839e94d
voyageartist <voyageartist@proton.me>
2023-01-29 18:01:59 +0000

    last upd.

--- python/scythe/MNIST.py
+++ python/scythe/MNIST.py
@@ -28,4 +28,9 @@ def loadMNISTTestSet(location = "."):
     y_filepath = os.path.join(location, "t10k-labels-idx1-ubyte")
     with open(y_filepath, "rb") as y_file:
         _, _ = struct.unpack(">II", y_file.read(8))
-        labels = n
+        labels = np.fromfile(y_file, dtype = np.int8)
+
+    X_filepath = os.path.join(location, "t10k-images-idx3-ubyte")
+    with open(X_filepath, "rb") as X_file:
+        _, _, instances, features = struct.unpack(">IIII", X_file.read(16))
+        images = np.fromfi
