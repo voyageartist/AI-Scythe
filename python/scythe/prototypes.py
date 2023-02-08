44b697c627e8754c47fc41729156ebd5b57b18b0
voyageartist <voyageartist@proton.me>
2023-02-08 11:55:34 +0000

    CC

--- python/scythe/prototypes.py
+++ python/scythe/prototypes.py
@@ -100,3 +100,148 @@ class APDTree:
 		for f in range(self.n_features):
 			bins = np.arange(self.discretization)
 			self.densities.append(np.digitize(X[:, f], bins))
+		boundaries = [[0, len(self.densities[f])] for f in range(self.n_features)]
+		return boundaries
+
+	def evaluateFeature(self, feature_id, X, bounds):
+		if bounds[0] == bounds[1]:
+			return np.inf, 0, None
+		split = self.densities[feature_id][random.choice(np.arange(bounds[0], bounds[1]))]
+		left_samples  = X[X[:, feature_id] <= split]
+		right_samples = X[X[:, feature_id] >  split]
+		n_left, n_right = len(left_samples), len(right_samples)
+		if n_left <= self.min_n_samples or n_right <= self.min_n_samples:
+			return np.inf, 0, None
+		left_cost, left_pca = self.fitPCA(left_samples)
+		right_cost, right_pca = self.fitPCA(right_samples)
+
+		cost = n_left * left_cost ** 2 + n_right * right_cost ** 2
+		return cost, split, (left_pca, right_pca)
+
+	def fit(self, X):
+		boundaries = self.preprocessDensities(X) #TODO
+		n_samples = X.shape[0]
+		self.sample_mask = np.zeros(n_samples, dtype = np.int)
+		self.root = Node(0)
+		self.n_nodes = 1
+		queue = Queue()
+		queue.put(self.root)
+
+		while not queue.empty():
+			current_node = queue.get()
+			samples = X[self.sample_mask == current_node.node_id]
+
+			is_left, mu, hyperplane, threshold = self.splitNode(samples)
+			current_node.hyperplane = hyperplane
+			current_node.mu = mu
+			current_node.threshold = threshold
+
+			cost, eigenvectors = self.fitPCA(samples)
+			current_node.eigenvectors = eigenvectors
+
+			if len(samples) >= self.min_n_samples:
+				left_mask = np.zeros(len(X), dtype = np.bool)
+				left_mask[self.sample_mask == current_node.node_id] = is_left
+				right_mask = np.zeros(len(X), dtype = np.bool)
+				right_mask[self.sample_mask == current_node.node_id] = ~is_left
+				self.sample_mask[left_mask]  = self.n_nodes
+				self.sample_mask[right_mask] = self.n_nodes + 1
+
+				current_node.left_child  = Node(self.n_nodes)
+				current_node.right_child = Node(self.n_nodes + 1)
+				queue.put(current_node.left_child)
+				queue.put(current_node.right_child)
+				self.n_nodes += 2
+
+	def encode_with_root(self, X):
+		return np.dot(X, self.root.eigenvectors)
+
+	def decode_with_root(self, X):
+		return np.dot(X, self.root.eigenvectors.T)
+
+	def encode(self, X):
+		X = np.asarray(X)
+		n_samples = X.shape[0]
+		result = np.empty((n_samples, self.n_principal_components), dtype = X.dtype)
+		for i in range(n_samples):
+			current_node = self.root
+			while current_node is not None:
+				child = current_node.whichChild(X[i, :])
+				if child == NONE:
+					break
+				elif child == LEFT:
+					current_node = current_node.left_child
+				else:
+					current_node = current_node.right_child
+			eigvecs = current_node.eigenvectors
+			# eigvecs = self.root.eigenvectors
+			result[i, :] = np.dot(X[i, :], eigvecs)
+		return result
+
+	def decode(self, X):
+		X = np.asarray(X)
+		n_samples = X.shape[0]
+		result = np.empty((n_samples, self.n_features), dtype = X.dtype)
+		for i in range(n_samples):
+			current_node = self.root
+			while True:
+				d = np.dot(X[i, :], current_node.eigenvectors.T)
+				child = current_node.whichChild(d)
+				if child == NONE:
+					break
+				elif child == LEFT:
+					current_node = current_node.left_child
+				else:
+					current_node = current_node.right_child
+			# eigvecs = self.root.eigenvectors
+			eigvecs = current_node.eigenvectors
+			result[i, :] = np.dot(X[i, :], eigvecs.T)
+		return result
+
+
+if __name__ == "__main__":
+	from sklearn.decomposition import PCA
+	from sklearn.datasets import make_classification
+	import matplotlib.pyplot as plt
+
+	from scythe.MNIST import *
+
+	n_samples  = 5000
+	n_features = 20
+	n_classes  = 10
+	X, y = make_classification(
+		n_samples     = 2 * n_samples, 
+		n_informative = 7,
+		n_features    = 20,
+		n_classes     = 10)
+
+	tree = APDTree(n_principal_components = 2, min_n_samples = 500, n_iterations = 8)
+	tree.fit(X)
+	print(tree.n_nodes)
+	sc_encoded = tree.encode_with_root(X)
+	sc_decoded = tree.decode_with_root(sc_encoded)
+	print("Scythe encoding error : %f" % APDTree.computeCost(X, sc_decoded))
+	
+	pca = PCA(n_components = 2)
+	pca.fit(X)
+	sk_encoded = pca.transform(X)
+	sk_decoded = pca.inverse_transform(sk_encoded)
+	print("Sklearn encoding error: %f" % APDTree.computeCost(X, sk_decoded))
+
+	def scatterPlot(encoded, title = ""):
+		colors = ["red", "yellow", "cyan", "black", "brown", "blue", "green", "purple", "orange", "orangered"]
+		for c in range(n_classes):
+			samples = encoded[y == c, :]
+			plt.scatter(samples[:, 0], samples[:, 1], c = colors[c])
+			plt.xlabel("Principal component 1")
+			plt.ylabel("Principal component 2")
+			plt.title(title)
+
+	plt.figure(1)
+	plt.subplot(121)
+	scatterPlot(sc_encoded, "Scythe APD tree")
+	plt.subplot(122)
+	scatterPlot(sk_encoded, "Scikit-learn PCA")
+	plt.show()
+
+	print("Finished")
---
+++ python/scythe/setup.py
@@ -0,0 +1,85 @@
+
+# -*- coding: utf-8 -*-
+
+import os, sys
+from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
+
+import numpy as np
+from numpy.distutils.misc_util import Configuration
+from numpy.distutils.core import setup as np_setup
+
+
+LANGUAGE = "c++"
+CPP_SRC_FOLDER = "../../src"
+
+cpp_src_files = [
+    (
+        "MNIST.py", [], "MNIST"
+    ),
+    (
+        "utils.py", [], "utils"
+    ),
+    (
+        "plot.py", [], "plot"
+    ),
+    (
+        "tools/encoders.cpp", [], "encoders",
+    ),
+    (
+        "core.cpp",
+        [
+            "densities/continuous.cpp",
+            "forest/classification_rf.cpp",
+            "forest/forest.cpp",
+            "misc/bagging.cpp",
+            "misc/sets.cpp",
+            "tree/cart.cpp",
+            "tree/heuristics.cpp",
+            "tree/pruning.cpp",
+            "scythe.cpp",
+            "deep_learning/layers/layer.cpp",
+            "deep_learning/layers/concatenation_layer.cpp",
+            "deep_learning/layers/scanner2D.cpp",
+            "deep_learning/deep_forest.cpp",
+            "deep_scythe.cpp"
+        ],
+        "core"
+    ),
+]
+
+source_files = list()
+for module in cpp_src_files:
+    cpp_src_filepaths = [module[0]]
+    for filename in module[1]:
+        cpp_src_filepaths.append(os.path.join(CPP_SRC_FOLDER, filename))
+    source_files.append((cpp_src_filepaths, module[2]))
+
+
+source_folder = "scythe"
+sub_packages = []
+
+
+COMPILER = "mingw"
+
+if COMPILER == "mingw":
+    extra_compile_args = [
+        "-std=c++14", 
+        "-ftree-loop-optimize",
+        "-ftree-vectorize",
+        "-funroll-loops",
+        "-ftree-vectorizer-verbose=1",
+        "-g",
+        # "-fopenmp",
+        # "-fopenmp-simd"
+        "-Iinclude",
+        "-O3"
+    ]
+elif COMPILER == "msvc":
+    # https://msdn.microsoft.com/en-us/library/fwkeyyhe.aspx
+    extra_compile_args = [
+        "/O2",
+        "/arch",
+        "/GA",
+        "/Wall"
+    ]
