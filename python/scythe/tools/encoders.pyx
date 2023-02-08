cd37bcb29480c154d6e7f3e1229f42a512afce29
voyageartist <voyageartist@proton.me>
2023-02-08 17:09:14 +0000

    Splash

--- python/scythe/tools/encoders.pyx
+++ python/scythe/tools/encoders.pyx
@@ -8,4 +8,15 @@ import numpy as np
 cimport numpy as cnp
 cnp.import_array()
 
-from libc.stdlib cimp
+from libc.stdlib cimport malloc, free
+from libc.stdio cimport *
+from libc.string cimport strcmp
+
+from cpython.unicode cimport *
+
+
+cdef cnp.int32_t FNV_32_PRIME = 16777619
+cdef cnp.int64_t FNV_64_PRIME = 1099511628211
+
+
+cdef inline cnp.int32
