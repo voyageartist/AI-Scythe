cf644245fbb283f9dce5821b6b960db460471c58
voyageartist <voyageartist@proton.me>
2023-02-14 18:59:34 +0000

    clean ups

--- python/tests/df_test.py
+++ python/tests/df_test.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
-# df_test.py: Tests on the de
+# df_test.py: Tests on the deep forest
+# author : Antoine Passemiers
+
+import os, sys
+
+import matplotlib.pyplot as plt
+
+from scythe.core import *
+from scythe.layers import *
+
+def minimal_test():
+    n_forests_per_layer = 2
+    kc, kr = 2, 2
+
+    fconfig = Fores
