881f80d01826f98b71b0553ff5b6b19de2edaff0
voyageartist <voyageartist@proton.me>
2023-02-26 18:27:26 +0000

    Reorganize file structure

--- python/visualisation/conv3D.py
+++ python/visualisation/conv3D.py
@@ -14,4 +14,12 @@ class Iterator:
         self.O = O
         self.kc = kc
         self.kr = kr
-       
+        self.kp = kp
+        self.sc = self.P - self.kc + 1
+        self.sr = self.M - self.kr + 1
+        self.sp = self.O - self.kp + 1
+
+    def _iterator_begin(self, j):
+        self.i = 0
+        self.q = 0
+        self.x = self.P * (j
