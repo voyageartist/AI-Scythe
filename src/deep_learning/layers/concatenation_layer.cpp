1418e3b7e2355c7db97444bf5dbb0e83becfc575
voyageartist <voyageartist@proton.me>
2023-03-25 19:43:47 +0000

    ext4: fix hang when processing corrupted orphaned inode list

--- src/deep_learning/layers/concatenation_layer.cpp
+++ src/deep_learning/layers/concatenation_layer.cpp
@@ -80,4 +80,10 @@ void ConcatenationDataset::allocateFromSampleMask(
 
     uint k = 0;
     inline_iterator_begin(feature_id);
-    for (ui
+    for (uint i = 0; i < n_instances; i++) {
+        if (sample_mask[i] == node_id) {
+            t_contiguous_data[k++] = static_cast<fast_data_t>(inline_iterator_deref());
+        }
+        inline_iterator_inc();
+    }
+    contiguous_d
