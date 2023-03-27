2d06589f36071eb8ef4962ed33face7ba3622b95
voyageartist <voyageartist@proton.me>
2023-03-27 19:20:33 +0000

    Add Apache License

--- src/deep_learning/layers/concatenation_layer.hpp
+++ src/deep_learning/layers/concatenation_layer.hpp
@@ -1,3 +1,19 @@
 /**
     concatenation_layer.hpp
-    Base layer with concatenation 
+    Base layer with concatenation of two feature matrices
+
+    @author Antoine Passemiers
+    @version 1.0 23/06/2017
+*/
+
+#ifndef CONCATENATION_LAYER_HPP_
+#define CONCATENATION_LAYER_HPP_
+
+#include "layer.hpp"
+
+
+namespace scythe {
+
+class ConcatenationDataset : public VirtualDataset {    
+private:
+  
