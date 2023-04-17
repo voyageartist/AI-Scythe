a96976d14530b9e41cca5aa30b4d8cb8d6010151
voyageartist <voyageartist@proton.me>
2023-04-17 10:54:21 +0000

    workqueue: perform cpu down operations from low priority cpu_notifier()

--- src/deep_learning/layers/scanner2D.cpp
+++ src/deep_learning/layers/scanner2D.cpp
@@ -164,3 +164,91 @@ void ScannedTargets2D::allocateFromSampleMask(
         @param sample_mask
             Pointer indicating for each data sample the id of the node it belongs to
         @param node_id
+            Id of the current node
+        @param n_items
+            Number of data samples belonging to the current node
+        @param n_instances
+            Number of data samples in the whole dataset
+    */
+    if (n_items != this->n_contiguous_items) { // TODO
+        if (contiguous_labels != nullptr) {
+            delete[] contiguous_labels;
+        }
+        contiguous_labels = new label_t[n_items];
+        this->n_contiguous_items = n_items;
+    }
+    uint k = 0;
+    _it_x = _it_i = 0;
+    for (uint i = 0; i < n_instances; i++) {
+        if (sample_mask[i] == node_id) {
+            contiguous_labels[k++] = data[_it_i];
+        }
+        if (++_it_x == s) {
+            _it_i++;
+            _it_x = 0;
+        }
+    }
+    assert(k == n_items);
+}
+
+void ScannedTargets2D::_iterator_begin() {
+    _it_x = _it_i = 0;
+}
+
+void ScannedTargets2D::_iterator_inc() {
+    if (++_it_x == s) {
+        _it_i++;
+        _it_x = 0;
+    }
+}
+
+target_t ScannedTargets2D::_iterator_deref() {
+    return data[_it_i];
+}
+
+target_t ScannedTargets2D::operator[](const size_t i) {
+    return data[i / s];
+}
+
+MultiGrainedScanner2D::MultiGrainedScanner2D(
+    LayerConfig lconfig, size_t kc, size_t kr) : Layer(lconfig), kc(kc), kr(kr) {}
+
+vdataset_p MultiGrainedScanner2D::virtualize(MDDataset dataset) {
+    assert(dataset.n_dims == 3);
+    assert(dataset.dims[0] > 0);
+    assert(dataset.dims[1] > 0);
+    assert(dataset.dims[2] > 0);
+    Layer::vdataset = std::shared_ptr<ScannedDataset2D>(
+        new ScannedDataset2D(
+            static_cast<data_t*>(dataset.data), // TODO : type erasure
+            dataset.dims[0],
+            dataset.dims[1],
+            dataset.dims[2],
+            this->kc,
+            this->kr,
+            dataset.dtype));
+    return Layer::vdataset;
+}
+
+vtargets_p MultiGrainedScanner2D::virtualizeTargets(Labels* targets) {
+    ScannedDataset2D* vdataset = dynamic_cast<ScannedDataset2D*>((this->vdataset).get());
+    size_t sc = vdataset->getSc();
+    size_t sr = vdataset->getSr();
+    size_t n_rows = vdataset->getNumRows();
+    assert(sc > 0);
+    assert(sr > 0);
+    return std::shared_ptr<ScannedTargets2D>(
+        new ScannedTargets2D(targets->data, n_rows, sc, sr));
+}
+
+size_t MultiGrainedScanner2D::getNumVirtualFeatures() {
+    ScannedDataset2D* sdataset = dynamic_cast<ScannedDataset2D*>(vdataset.get());
+    size_t n_vfeatures = sdataset->getSc() * sdataset->getSr();
+    assert(n_vfeatures > 0);
+    if (lconfig.fconfig.task == CLASSIFICATION_TASK) {
+        n_vfeatures *= lconfig.fconfig.n_classes;
+    }
+    return n_vfeatures * lconfig.n_forests;
+}
+
+} // namespace
---
+++ src/deep_learning/layers/scanner2D.hpp
@@ -0,0 +1,12 @@
+/**
+    scanner2D.hpp
+    Multi-grained scanning
+
+    @author Antoine Passemiers
+    @version 1.0 10/06/2017
+*/
+
+#ifndef SCANNER2D_HPP_
+#define SCANNER2D_HPP_
+
+#include "../../misc/sets
