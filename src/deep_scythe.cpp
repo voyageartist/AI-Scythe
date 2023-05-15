5629611ec8b4e959b85f7be9c81a7832258da59a
voyageartist <voyageartist@proton.me>
2023-05-15 13:01:50 +0000

    some corrections

--- src/deep_scythe.cpp
+++ src/deep_scythe.cpp
@@ -1,3 +1,15 @@
 /**
     deep_scythe.cpp
-    Scythe's deep learn
+    Scythe's deep learning C API
+
+    @author Antoine Passemiers
+    @version 1.0 10/06/2017
+*/
+
+#include "deep_scythe.hpp"
+
+
+CppClassesInterface cpp_classes_interface = CppClassesInterface();
+
+scythe::DeepForest* CppClassesInterface::get(size_t i) {
+   
