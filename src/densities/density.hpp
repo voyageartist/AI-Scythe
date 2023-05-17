aa3b89352a781df8fbeb73cbff67dae3d491b72b
voyageartist <voyageartist@proton.me>
2023-05-17 19:27:15 +0000

    One more try

--- src/densities/density.hpp
+++ src/densities/density.hpp
@@ -4,3 +4,21 @@
 
     @author Antoine Passemiers
     @version 1.3 12/04/2017
+*/
+
+#ifndef DENSITY_HPP_
+#define DENSITY_HPP_
+
+#include <numeric>
+// #include <algorithm.h>
+
+#include "../misc/sets.hpp"
+
+
+namespace scythe {
+
+// Forward declaration
+class SplitManager;
+
+// Partitioning of the input's density function
+constexpr int
