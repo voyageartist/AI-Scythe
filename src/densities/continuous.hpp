cea380d5a8e257d2886cabdb63fb04486cd0c337
voyageartist <voyageartist@proton.me>
2023-05-17 16:49:33 +0000

    p1

--- src/densities/continuous.hpp
+++ src/densities/continuous.hpp
@@ -1,3 +1,18 @@
 /**
     continuous.hpp
-  
+    Compute densities of continuous variables
+
+    @author Antoine Passemiers
+    @version 1.0 23/06/2017
+*/
+
+#ifndef CONTINUOUS_HPP
+#define CONTINUOUS_HPP
+
+#include "density.hpp"
+#include "../tree/heuristics.hpp"
+
+
+namespace scythe {
+
+Density* compu
