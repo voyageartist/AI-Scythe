ace3efd2fcc681a5b49dd6595c06c59cd2b5f461
voyageartist <voyageartist@proton.me>
2023-05-18 20:28:24 +0000

    Fix tslint errors

--- src/densities/grayscale.hpp
+++ src/densities/grayscale.hpp
@@ -1,3 +1,17 @@
 /**
     grayscale.hpp
-    Compu
+    Compute pixel value densities
+
+    @author Antoine Passemiers
+    @version 1.0 23/06/2017
+*/
+
+#ifndef GRAYSCALE_HPP
+#define GRAYSCALE_HPP
+
+#include "density.hpp"
+
+
+namespace scythe {
+
+Density* getArbitraryPixelDensities(size
