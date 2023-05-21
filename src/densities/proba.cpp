ae55de3386380cdd1a72ce1756f7813bcbbf0c76
voyageartist <voyageartist@proton.me>
2023-05-21 10:25:11 +0000

    Deleted files manually

--- src/densities/proba.cpp
+++ src/densities/proba.cpp
@@ -9,4 +9,10 @@
 #include "proba.hpp"
 
 
-namespace 
+namespace scythe {
+
+Density* getArbitraryProbaDensities(size_t n_features, size_t n_classes) {
+    assert(n_features > 0);
+    Density* densities = new Density[n_features];
+    densities[0].counters_left = new size_t[n_classes];
+    densities[0].counters_right = new 
