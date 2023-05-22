483691ed656614712b284af025fffd0352e4a994
voyageartist <voyageartist@proton.me>
2023-05-22 21:50:12 +0000

    Comment fixed

--- src/deprecated/classification_complete_rf.cpp
+++ src/deprecated/classification_complete_rf.cpp
@@ -1,2 +1,14 @@
 /**
-    classificatio
+    classification_complete_rf.cpp
+    Classification completely-random forests
+
+    @author Antoine Passemiers
+    @version 1.0 13/06/2017
+*/
+
+#include "classification_complete_rf.hpp"
+
+
+namespace scythe {
+
+ClassificationCompleteRF::Classificati
