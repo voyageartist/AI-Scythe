a3e5e3b724ce20c5308515be47fdc6bf359f4c3f
voyageartist <voyageartist@proton.me>
2023-06-06 12:04:43 +0000

    Admin page

--- src/deprecated/scanner1D.hpp
+++ src/deprecated/scanner1D.hpp
@@ -3,4 +3,18 @@
     Multi-grained scanning
 
     @author Antoine Passemiers
-    @version 1.0
+    @version 1.0 10/06/2017
+*/
+
+#ifndef SCANNER1D_HPP_
+#define SCANNER1D_HPP_
+
+#include "../../misc/sets.hpp"
+#include "layer.hpp"
+
+namespace scythe {
+
+class ScannedDataset1D : public VirtualDataset {
+private:
+    size_t N;  // Number of instances
+    size_t 
