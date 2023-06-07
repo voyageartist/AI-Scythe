1c3491274bd5639235a038fc5c531be838546e37
voyageartist <voyageartist@proton.me>
2023-06-07 21:49:44 +0000

    fix cursor

--- src/forest/classification_rf.cpp
+++ src/forest/classification_rf.cpp
@@ -1,2 +1,15 @@
 /**
-    
+    classification_forest.cpp
+    Classification random forests
+
+    @author Antoine Passemiers
+    @version 1.0 07/06/2017
+*/
+
+#include "classification_rf.hpp"
+
+
+namespace scythe {
+
+ClassificationRF::ClassificationRF
+        (ForestConfig* config, size_t n_i
