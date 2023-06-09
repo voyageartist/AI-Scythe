29a1d60e40581a77797d332a93115beb65e3430c
voyageartist <voyageartist@proton.me>
2023-06-09 07:57:54 +0000

    Fix #119

--- src/forest/classification_rf.cpp
+++ src/forest/classification_rf.cpp
@@ -15,4 +15,10 @@ ClassificationRF::ClassificationRF
         (ForestConfig* config, size_t n_instances, size_t n_features) :
         ClassificationForest::ClassificationForest(config, n_instances, n_features) {
     Forest::base_tree_config.task = CLASSIFICATION_TASK;
-    Forest::base_tree_config.is_complete_random = (config->type == COMPLETE_RANDOM_FOREST); 
+    Forest::base_tree_config.is_complete_random = (config->type == COMPLETE_RANDOM_FOREST);   
+    if ((Forest::base_tree_config.max_n_features > n_features) ||
+        (Forest::base_tree_config.max_n_features == 0)) {
+        Forest::base_tree_config.max_n_features = n_features;
+    }
+    /*
+    this->score_m
