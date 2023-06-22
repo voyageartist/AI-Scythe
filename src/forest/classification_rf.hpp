8259571ee64c8927414c64aec06a26d5848e4a1f
voyageartist <voyageartist@proton.me>
2023-06-22 11:04:51 +0000

    Add type

--- src/forest/classification_rf.hpp
+++ src/forest/classification_rf.hpp
@@ -3,4 +3,18 @@
     Classification random forests
     
     @author Antoine Passemiers
-    @ver
+    @version 1.0 12/04/2017
+*/
+
+#ifndef CLASSIFICATION_RF_HPP_
+#define CLASSIFICATION_RF_HPP_
+
+#include "../misc/sets.hpp"
+#include "forest.hpp"
+#include "../metrics/classification_metrics.hpp"
+#include "../misc/bagging.hpp"
+
+
+namespace scythe {
+
+class C
