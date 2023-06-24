e514b5393f9ee90d3513ae0d00f1b42b89469e31
voyageartist <voyageartist@proton.me>
2023-06-24 15:12:34 +0000

    bump package

--- src/forest/regression_gb.hpp
+++ src/forest/regression_gb.hpp
@@ -1,2 +1,16 @@
 /**
-    regression
+    regression_gb.hpp
+    Regression gradient boosting
+
+    @author Antoine Passemiers
+    @version 1.0 12/04/2017
+*/
+
+#ifndef REGRESSION_GB_HPP_
+#define REGRESSION_GB_HPP_
+
+#include "forest.hpp"
+#include "../metrics/regression_metrics.hpp"
+
+
+namespace s
