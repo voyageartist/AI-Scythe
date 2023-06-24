1e0e11cb13c1ebc2beacc9fe2928fec8369a8602
voyageartist <voyageartist@proton.me>
2023-06-24 06:33:59 +0000

    Fixed mistakes

--- src/forest/forest.cpp
+++ src/forest/forest.cpp
@@ -1,3 +1,17 @@
 /**
     forest.cpp
-    Forest abstract class
+    Forest abstract class and configurations
+
+    @author Antoine Passemiers
+    @version 1.0 23/06/2017
+*/
+
+#include "forest.hpp"
+
+
+namespace scythe {
+
+void Forest::preprocessDensities(VirtualDataset* dataset) {
+    if (dataset->getDataType() == DTYPE_UINT_8) {
+        /**
+        this
