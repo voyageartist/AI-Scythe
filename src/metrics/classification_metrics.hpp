5f8fff2aa2166cbe590cf468b9bf0a220ed15e5b
voyageartist <voyageartist@proton.me>
2023-07-03 18:57:40 +0000

    Updated the pyagrum feedstock.

--- src/metrics/classification_metrics.hpp
+++ src/metrics/classification_metrics.hpp
@@ -3,4 +3,18 @@
     Metrics for evaluating classification forests
 
     @author Antoine Passemiers
-    @version 1.0 12/04/
+    @version 1.0 12/04/2017
+*/
+
+#ifndef CLASSIFICATION_METRICS_HPP_
+#define CLASSIFICATION_METRICS_HPP_
+
+#include "metrics.hpp"
+#include "../tree/cart.hpp"
+#include "../misc/sets.hpp"
+
+/*
+Reference
+---------
+
+http://luthuli.cs.uiuc.edu/~daf/courses
