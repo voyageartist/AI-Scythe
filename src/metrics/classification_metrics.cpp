1bb0c28a92032421735729672e9fedcfeda5d473
voyageartist <voyageartist@proton.me>
2023-07-03 07:50:39 +0000

    Corrected docs

--- src/metrics/classification_metrics.cpp
+++ src/metrics/classification_metrics.cpp
@@ -7,3 +7,12 @@
 */
 
 #include "classification_metrics.hpp"
+
+
+namespace scythe {
+
+ClassificationError::ClassificationError() : 
+    n_classes(0), n_instances(0), gradient(nullptr) {}
+
+ClassificationError::ClassificationError(size_t n_classes, size_t n_instances) : 
+    n_classes(n_classes),
