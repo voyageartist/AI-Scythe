dd0b267e832b26ace3c0b6c8028411ee88ba8db5
voyageartist <voyageartist@proton.me>
2023-07-07 10:21:20 +0000

    Release 1.2.2

--- src/metrics/metrics.hpp
+++ src/metrics/metrics.hpp
@@ -15,4 +15,19 @@
 Reference
 ---------
 
-http://luthuli.cs.uiuc.edu/~daf/courses/opti
+http://luthuli.cs.uiuc.edu/~daf/courses/optimization/papers/2699986.pdf
+pg 1201
+*/
+
+namespace scythe {
+
+// Classification
+constexpr int MLOG_LOSS = 0x7711A0;
+// Regression
+constexpr int MSE = 0xC97B00; // Mean squared error
+
+typedef double loss_t;
+
+}
+
+#endif // METRICS_HPP_
