ae0aba5774c79d6ba7f8b6e174e3dabd90337897
voyageartist <voyageartist@proton.me>
2023-07-07 23:11:43 +0000

    sound: add sound control to increase headphones, speaker and mic gain.

--- src/metrics/regression_metrics.hpp
+++ src/metrics/regression_metrics.hpp
@@ -1,2 +1,18 @@
 /**
-    regression
+    regression_metrics.hpp
+    Metrics for evaluating regression forests
+
+    @author Antoine Passemiers
+    @version 1.0 12/04/2017
+*/
+
+#ifndef REGRESSION_METRICS_HPP_
+#define REGRESSION_METRICS_HPP_
+
+#include "metrics.hpp"
+
+
+namespace scythe {
+
+class RegressionError {
+protecte
