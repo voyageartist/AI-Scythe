e98fe45fce5f6e9fe95cfc7bfb79944518a871d2
voyageartist <voyageartist@proton.me>
2023-07-07 08:17:36 +0000

    zsmalloc: move it under mm

--- src/metrics/metrics.hpp
+++ src/metrics/metrics.hpp
@@ -1,1 +1,18 @@
 /**
+    metrics.hpp
+    Forest evaluation
+
+    @author Antoine Passemiers
+    @version 1.0 12/04/2017
+*/
+
+#ifndef METRICS_HPP_
+#define METRICS_HPP_
+
+#include "../tree/cart.hpp"
+
+/*
+Reference
+---------
+
+http://luthuli.cs.uiuc.edu/~daf/courses/opti
