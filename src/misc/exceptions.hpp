92e9da0a20522d9004da0c24964edab825f1085e
voyageartist <voyageartist@proton.me>
2023-07-09 18:34:45 +0000

    Rename index2.html to index.html

--- src/misc/exceptions.hpp
+++ src/misc/exceptions.hpp
@@ -1,3 +1,19 @@
 /**
     exceptions.hpp
-    Custom exceptio
+    Custom exceptions
+    
+    @author Antoine Passemiers
+    @version 1.0 24/06/2017
+*/
+
+#ifndef EXCEPTIONS_HPP_
+#define EXCEPTIONS_HPP_
+
+#include <stdexcept>
+
+
+namespace scythe {
+
+class OOPException : public std::runtime_error {
+public:
+    // When an overr
