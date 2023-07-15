478f8c398117757292783ce27db4d2b6dc953430
voyageartist <voyageartist@proton.me>
2023-07-15 20:28:16 +0000

    Initial creation.

--- src/misc/utils.hpp
+++ src/misc/utils.hpp
@@ -7,4 +7,21 @@
 */
 
 #include <cassert>
-#include <cstd
+#include <cstdint>
+
+#include "numpy/npy_common.h"
+#include "numpy/noprefix.h"
+
+#include "exceptions.hpp"
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+
+namespace scythe {
+
+typedef uint8_t BYTE;
+
+constexpr int NPY_BOOL_NUM     =  0;
+constexpr int NPY_INT8_NUM     =  1;
+co
