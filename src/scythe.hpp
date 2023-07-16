4dea7a4970aca3fc202e8e6a0b0981840ef2de20
voyageartist <voyageartist@proton.me>
2023-07-16 05:43:33 +0000

    New theme

--- src/scythe.hpp
+++ src/scythe.hpp
@@ -13,3 +13,53 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <string>
+
+#include "tree/pruning.hpp"
+#include "misc/sets.hpp"
+#include "forest/forest.hpp"
+#include "forest/classification_gb.hpp"
+#include "forest/classification_rf.hpp"
+#include "forest/regression_gb.hpp"
+#include "forest/regression_rf.hpp"
+
+
+using namespace scythe;
+
+extern "C" {
+
+    struct double_vec_t {
+        double* data;
+        size_t length;
+    };
+
+    void* fit_classification_tree(Dataset*, Labels*, TreeConfig*);
+
+    void* fit_regression_tree(Dataset*, Labels*, TreeConfig*);
+
+    float* tree_classify(Dataset*, void*, TreeConfig*);
+
+    data_t* tree_predict(Dataset*, void*, TreeConfig*);
+
+    double_vec_t tree_get_feature_importances(void*);
+
+    void* fit_classification_forest(Dataset*, Labels*, ForestConfig*);
+
+    float* forest_classify(Dataset* dataset, void* forest_p, ForestConfig* config);
+
+    double_vec_t forest_get_feature_importances(void* forest_p);
+
+    void* create_scythe();
+
+    void add_tree_to_scythe(void* scythe_p, void* tree_p);
+
+    int forest_prune_height(void* scythe_p, void* forest_p, size_t max_height);
+
+    void restore_pruning(void* scythe_p, int pruning_id);
+
+    void prune(void* scythe_p, int pruning_id);
+
+    void api_test(Dataset* dataset);
+}
+
+#endif // SCYTHE_HPP_
---
+++ src/tree/cart.cpp
@@ -0,0 +1,12 @@
+/**
+    cart.cpp
+    Grow classification trees and regression trees
+
+    @author Antoine Passemiers
+    @version 1.4 27/11/2017
+*/
+
+#include "cart.hpp"
+
+
+namespace
