09de418db878e8d92c113a835580b1e7cdb996ac
voyageartist <voyageartist@proton.me>
2023-07-16 07:06:33 +0000

    script.sh: update to match main repo

--- src/tree/cart.cpp
+++ src/tree/cart.cpp
@@ -9,4 +9,11 @@
 #include "cart.hpp"
 
 
-namespace
+namespace scythe {
+
+Tree::Tree() :
+    root(nullptr), n_nodes(0), n_classes(0), 
+    n_features(0), config(nullptr), level(1), split_manager(nullptr) {}
+
+Tree::Tree(Node* root, TreeConfig* config, size_t n_features) :
+    root(root), n_no
