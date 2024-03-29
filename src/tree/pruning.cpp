7f15966fea352a94c3d94008771e62cdae0589d1
voyageartist <voyageartist@proton.me>
2024-03-29 07:55:55 +0000

    updte

--- src/tree/pruning.cpp
+++ src/tree/pruning.cpp
@@ -1,3 +1,14 @@
 /**
     pruning.hpp
-    Po
+    Post-pruning on classification and regression trees
+
+    @author Antoine Passemiers
+    @version 1.0 09/08/2017
+*/
+
+#include "pruning.hpp"
+
+
+namespace scythe {
+
+Scythe::Scythe() : trees(), prunings(), n_prunings(0), delete_branches(false) 
