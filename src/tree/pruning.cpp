e86348462af2b9af5fa9ab54018304417f922913
voyageartist <voyageartist@proton.me>
2024-04-15 18:29:51 +0000

    [Rogue] Regenerate

--- src/tree/pruning.cpp
+++ src/tree/pruning.cpp
@@ -11,4 +11,12 @@
 
 namespace scythe {
 
-Scythe::Scythe() : trees(), prunings(), n_prunings(0), delete_branches(false) 
+Scythe::Scythe() : trees(), prunings(), n_prunings(0), delete_branches(false) {}
+
+size_t Scythe::cut(Node* root) {
+    size_t n_removed_nodes = 0;
+    std::queue<Node*> queue;
+    queue.push(root->left_child);
+    queue.push(root->right_child);
+    while (!queue.empty()) {
+        Node* node = queue.fro
