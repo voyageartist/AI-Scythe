1cb9b08c7ae73c49bf310544d135c103de6c86db
voyageartist <voyageartist@proton.me>
2024-07-06 05:44:19 +0000

    Updated background

--- src/tree/pruning.hpp
+++ src/tree/pruning.hpp
@@ -1,3 +1,20 @@
 /**
     pruning.hpp
-  
+    Post-pruning on classification and regression trees
+
+    @author Antoine Passemiers
+    @version 1.0 09/08/2017
+*/
+
+#ifndef PRUNING_HPP_
+#define PRUNING_HPP_
+
+#include "../tree/cart.hpp"
+
+
+namespace scythe {
+
+
+struct NodeLevel {
+    Node* owner;
+    
